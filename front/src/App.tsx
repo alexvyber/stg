8b6839039193fba05c4ba32d583ef34ab976ce36
Alexey Sokolov <alexvyber@gmail.com>
2022-12-10 09:48:27 +0300

    basic ui

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,9 +1,19 @@
+import { useState } from "react"
 import "./App.css"
 
 function App() {
+  const [state, setState] = useState("")
   return (
     <div className="App">
       <h1>Vite + React</h1>
+      <div className="main">
+        <input
+          type="text"
+          value={state}
+          onChange={({ target }) => setState(target.value)}
+        />
+        <button>Click!</button>
+      </div>
     </div>
   )
 }
