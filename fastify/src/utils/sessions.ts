5d95758ebdd3155079529dc8980dc2f50fc487f1
Alexey Sokolov <alexvyber@gmail.com>
2022-12-11 15:32:20 +0300

    create session and save in db

---
+++ fastify/src/utils/sessions.ts
@@ -0,0 +1,39 @@
+import { ObjectId } from "mongodb"
+import { randomBytes } from "node:crypto"
+import { Fastify } from "../types/index.js"
+
+export async function createSession(
+  fastify: Fastify,
+  userId: ObjectId,
+  connection: any // FIXME: use appropriate type
+) {
+  try {
+    // Error in no db availible
+    if (!fastify.mongo.db) {
+      throw new Error("No db connection")
+    }
+
+    // Generate a session token
+    const sessionToken = randomBytes(64).toString("hex")
+
+    // retrieve connection information
+    const { ip, userAgent } = connection
+
+    const sessions = fastify.mongo.db.collection("sessions")
+
+    await sessions.insertOne({
+      sessionToken,
+      userId,
+      valid: true,
+      userAgent,
+      ip,
+      updatedAt: new Date(),
+      createdAt: new Date()
+    })
+
+    // Return session token
+    return sessionToken
+  } catch (e) {
+    throw new Error("Session Creation Failed")
+  }
+}
