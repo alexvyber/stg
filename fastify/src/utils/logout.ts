01b3df1116bc1e193ba3573badc634569959d2b1
Alexey Sokolov <alexvyber@gmail.com>
2022-12-11 16:09:42 +0300

    craete logout

---
+++ fastify/src/utils/logout.ts
@@ -0,0 +1,27 @@
+import type { FastifyReply } from "fastify/types/reply"
+import type { FastifyRequest } from "fastify/types/request"
+import { Fastify } from "../types"
+import { removeSession } from "./sessions"
+
+const jwtSecret = process.env.FASTIFY_JWT_SECRET || "super-secret"
+
+import { verify } from "jsonwebtoken"
+
+export async function logout(
+  fastify: Fastify,
+  request: FastifyRequest,
+  reply: FastifyReply
+) {
+  const { refreshToken } = request.cookies
+
+  if (!refreshToken) {
+    throw new Error("No refresh token provided")
+  }
+
+  const sessionToken = verify(refreshToken, jwtSecret)
+
+  await removeSession(fastify, sessionToken)
+
+  // Remove Cookies
+  reply.clearCookie("refreshToken").clearCookie("accessToken")
+}
