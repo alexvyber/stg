f9c5d6da686f35d92c9fdc5598e6f511b317eda4
Alexey Sokolov <alexvyber@gmail.com>
2022-12-11 13:50:04 +0300

    implemet basic registration

--- fastify/src/routes/register/index.ts
+++ fastify/src/routes/register/index.ts
@@ -1,26 +1,49 @@
 import { FastifyPluginAsync } from "fastify"
+import { UserData } from "../../entities/user"
+import { registerUser } from "../../utils/register"
 
 const register: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
-  fastify.get("/", async function (request, reply) {
-    if (!this.mongo.db) {
-      throw new Error("No db connection")
+  fastify.post<{ Body: UserData; Reply: any }>("/", async function (request, reply) {
+    // reply with 400 if user already logen in
+    if (request.cookies.accessToken || request.cookies.refreshToken) {
+      reply.code(400).send({
+        data: {
+          status: "FAILED",
+          message: "Already loged in"
+        }
+      })
     }
 
-    const users = this.mongo.db.collection("users")
+    const { email, password } = request.body
 
-    const res = await users.insertOne({
-      email: {
-        address: "email@asdfasdf.com",
-        verified: false
-      },
-      hashedPassword: "hashedPassword"
+    // reply with 400 if no password or email provided
+    if (!email || !password) {
+      reply.code(400).send({
+        data: {
+          status: "FAILED"
+        }
+      })
+    }
+
+    const userId = await registerUser(fastify, { email, password })
+
+    if (!userId) {
+      reply.code(400).send({
+        data: {
+          status: "FAILED",
+          message: "Failed to register"
+        }
+      })
+    }
+
+    reply.send({
+      data: {
+        status: "SUCCESS",
+        user: {
+          _id: userId
+        }
+      }
     })
-
-    request.log.info({
-      res
-    })
-
-    return res ? "success" : "failure"
   })
 }
 
---
+++ fastify/src/utils/register.ts
@@ -0,0 +1,35 @@
+// TODO: figure out propper way to place this code
+
+import type { UserData } from "../entities/user"
+import type { ObjectId } from "mongodb"
+
+import { genSalt, hash } from "bcryptjs"
+import { Fastify } from "../types"
+
+// HACK: hacky way to pass mongo with entire fastify object
+// ???: Is ii right?
+export const registerUser = async (fastify: Fastify, userData: UserData): Promise<ObjectId> => {
+  // Error in no db availible
+  if (!fastify.mongo.db) {
+    throw new Error("No db connection")
+  }
+
+  const users = fastify.mongo.db.collection("users")
+
+  // generate salt
+  const salt = await genSalt(10)
+
+  // hash with salt
+  const hashedPassword = await hash(userData.password, salt) // ???: learn more how salting affect hashinng spped
+
+  const res = await users.insertOne({
+    email: {
+      address: userData.email,
+      verified: false
+    },
+    hashedPassword
+  })
+
+  // Return user from database
+  return res.insertedId
+}
