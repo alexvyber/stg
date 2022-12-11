8d4acfc86b7a7aa6b21c089d399a421292c377ff
Alexey Sokolov <alexvyber@gmail.com>
2022-12-11 16:43:54 +0300

    configs...

---
+++ fastify/TODO.md
@@ -0,0 +1,6 @@
+# TODO:
+
+- learn the proper ways to organize code in fastify
+- create index for keys...
+- learn more about mongo
+- create schema
--- fastify/src/plugins/mongo.ts
+++ fastify/src/plugins/mongo.ts
@@ -2,8 +2,8 @@ import fp from "fastify-plugin"
 import { fastifyMongodb, type FastifyMongodbOptions } from "@fastify/mongodb"
 import { MongoClient } from "mongodb"
 
-// const url: "mongodb://root:example@mongo:27017/", // in container
-const url = "mongodb://root:example@localhost:27017/" // on localhost
+const url = "mongodb://root:example@mongo:27017/" // in container
+// const url = "mongodb://root:example@localhost:27017/" // on localhost
 
 export const client = new MongoClient(url)
 
@@ -18,7 +18,7 @@ export default fp<FastifyMongodbOptions>(async fastify => {
     // force to close the mongodb connection when app stopped
     // the default value is false
     client,
-    forceClose: true,
+    // forceClose: true,
     database: "fastify" // FIXME: do it in the propper way
   })
 })
